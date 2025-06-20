c62d3fc63692f5180796b6e6e01540eec14a0dea
Rishat <supera.2006@mail.ru>
2025-06-20 22:31:38 +0300

    Logo created

--- app/src/main/java/com/example/rentassistantapp/WelcomeScreen.kt
+++ app/src/main/java/com/example/rentassistantapp/WelcomeScreen.kt
@@ -1,2 +1,27 @@
 package com.example.rentassistantapp
 
+import androidx.compose.foundation.Image
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rentassistantapp.ui.theme.RentAssistantAppTheme
+
+@Composable
+fun WelcomeScreen(modifier: Modifier = Modifier) {
+    var logotype = painterResource(R.drawable.logo)
+
+    Image(
+        painter = logotype,
+        contentDescription = "Logo \"Rent Assistant\""
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RentAssistantAppTheme {
+        WelcomeScreen()
+    }
+}
