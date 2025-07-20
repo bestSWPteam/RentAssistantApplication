64c0bbde7bc498e04c8b5d1e6f74617ac3134267
Rishat <supera.2006@mail.ru>
2025-07-20 17:34:17 +0300

    fix: Changed bot address for authorization

--- app/src/main/java/com/example/rentassistantapp/util/Config.kt
+++ app/src/main/java/com/example/rentassistantapp/util/Config.kt
@@ -1,6 +1,6 @@
 package com.example.rentassistantapp.util
 
 object Config {
-    const val TELEGRAM_BOT_ID = "7497767876"
+    const val TELEGRAM_BOT_ID = "RentAnAssistantBot"
     const val API_BASE_URL = "https://rentassistant.ru/"
 }
